20460b7dd584c5fcf77d0bf3048ad4786d059086
SaeedS28 <awesomesaad.saeed2@gmail.com>
2018-06-27 00:28:59 -0400

    got the right file structure in place and got the relay board to send current to the led based on the digital output

---
+++ Phase-1/relayBoardOnly/relayBoardOnly.ino
@@ -0,0 +1,26 @@
+int led = 13;
+
+//5 to relay 2
+//6 to relay 1
+
+int relay1=6;
+int relay2=5;
+
+// the setup routine runs once when you press reset:
+void setup() {                
+  // initialize the digital pin as an output.
+  pinMode(led, OUTPUT);
+  pinMode(relay1, OUTPUT);
+  pinMode(relay2, OUTPUT);
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  //digitalWrite(relay1, HIGH);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(relay2, HIGH);
+  delay(5000);               // wait for a second
+  //digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
+  //digitalWrite(relay1, LOW);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(relay2, LOW);
+  delay(2000);               // wait for a second
+}
