9f953fc2ea3ea8501823cf186dac1aca4fa9064d
SaeedS28 <awesomesaad.saeed2@gmail.com>
2018-07-31 00:50:53 -0400

    modified the modbus packet to accept 4 parameters for encryption. Will test soon.

--- Phase-2/strikePlateCode/strikePlateCode.ino
+++ Phase-2/strikePlateCode/strikePlateCode.ino
@@ -1,54 +1,55 @@
 #include "Arduino.h"
 #include <AES.h>
 #define KEYBITS 256
-#define HWSERIAL Serial1
+//#define HWSERIAL Serial1
 #define HWSERIAL2 Serial2
 unsigned long rk[RKLENGTH(KEYBITS)]; //for decryption
 
 static int field = 0;
+int garbage1;
+int garbage2;
 int cipherKey;
 int nrounds;
-int crc;
+//int crc;
 int rede = 2;
-int message;
+//int message;
 
-static int listening = 0;
-
+static int listening = 0;   //not sure about this one
 
 void listenToSlave() {
-
-  if (HWSERIAL2.available() > 0) {
+  static int field = 0;
+  if (Serial2.available() > 0) {
     if (field == 0) {
-      cipherKey = int(HWSERIAL2.read());
-      //Serial.print("slave ID: ");
-      Serial.println(cipherKey);
+      garbage1 = int(Serial2.read());
+      Serial.print("Garbage 1: ");
+      Serial.println(garbage1);
       field++;
       listening = 1;
     }
     else if (field == 1) {
-      nrounds = int(HWSERIAL2.read());
-      //Serial.print("function ID: ");
-      Serial.println(nrounds);
+      garbage2= int(Serial2.read());
+      Serial.print("Garbage 2: ");
+      Serial.println(garbage2);
       field++;
       listening = 1;
     }
     else if (field == 2) {
-      message = int(HWSERIAL2.read());
-      //Serial.print("message: ");
-      //Serial.println(message);
+      cipherKey = int(Serial2.read());
+      Serial.print("CipherKey: ");
+      Serial.println(cipherKey);
       field++;
       listening = 1;
     }
     else if (field == 3) {
-      crc = int(HWSERIAL2.read());
-      //Serial.print("crc: ");
-      //Serial.println(crc);
+      nrounds = int(Serial2.read());
+      Serial.print("Number of rounds: ");
+      Serial.println(nrounds);
       field = 0;
       listening = 0;
     }
     else                    //flush the serial port
     {
-      int t = HWSERIAL2.read();
+      int t = Serial2.read();
       delay(200);
       field = 0;
       listening = 0;
@@ -56,7 +57,7 @@ void listenToSlave() {
   }
   else                      //flush the serial port
   {
-    int t = HWSERIAL2.read();
+    int t = Serial2.read();
     delay(200);
     field = 0;
   }
@@ -65,7 +66,7 @@ void listenToSlave() {
 
 void setup() {
   Serial.begin(9600);
-  Serial1.begin(9600);
+  //Serial1.begin(9600);
   Serial2.begin(9600);
   //HWSERIAL.begin(9600);
 
